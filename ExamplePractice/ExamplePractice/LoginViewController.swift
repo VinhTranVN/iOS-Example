7e88bafaf926ab1d95a93cda1331448683b02b4c
Vinh Tran <vlab.smartapps@gmail.com>
2017-07-05 22:55:28 +0700

    add library SwiftHTTP

--- ExamplePractice/ExamplePractice/LoginViewController.swift
+++ ExamplePractice/ExamplePractice/LoginViewController.swift
@@ -19,7 +19,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print(">>> LoginViewController viewDidLoad")
+        
     }
     
     override func didReceiveMemoryWarning() {
